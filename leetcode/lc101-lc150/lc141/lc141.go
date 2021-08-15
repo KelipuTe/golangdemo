029e482a456dbc60356eaac6f517e773c19740a1
KelipuTe <xhy_1365@sina.com>
2021-08-15 13:07:46 +0800

    leetcode

--- leetcode/lc101-lc150/lc141/lc141.go
+++ leetcode/lc101-lc150/lc141/lc141.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-  ln1 := ListNode{2, nil}
+  ln2 := ListNode{2, nil}
+  ln1 := ListNode{1, &ln2}
+  ln2.Next = &ln1
   fmt.Println(hasCycle(&ln1))
 }
 
@@ -14,21 +16,22 @@ type ListNode struct {
 
 //双指针
 //使用两个指针，一快一慢，快指针一次走两个结点，慢指针一次走一个结点
-//如果某个时刻两个指针指向的地址相等了，存在环形结构
+//如果某个时刻两个指针指向的地址相等了，则存在环形结构
 
 //141-环形链表
 func hasCycle(head *ListNode) bool {
-if head==nil{
-  return false
-}
-if head.Next==nil{
-  return false
-}
-tpln1=head.Next
-
-  for true {
-    tpln1 := head.Next.Next
-    tpln2 := head.Next
-    if
+  if head == nil || head.Next == nil {
+    return false
   }
+  tpln1, tpln2 := head, head.Next
+
+  for tpln1 != tpln2 {
+    if tpln2 == nil || tpln2.Next == nil {
+      return false
+    }
+    tpln1 = tpln1.Next
+    tpln2 = tpln2.Next.Next
+  }
+
+  return true
 }
