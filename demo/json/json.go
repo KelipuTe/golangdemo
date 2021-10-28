63d986c6c7ac7bf0a3778649fe631c014a8c11a5
KelipuTe <xhy_1365@sina.com>
2021-10-28 14:59:52 +0800

    demo

--- demo/json/json.go
+++ demo/json/json.go
@@ -7,11 +7,18 @@ import (
 
 func main() {
   fmt.Println("demo run")
-  jsonEncode()
-  jsonDecodeIntoMap()
-  jsonDecodeIntoStruct()
+  StractEncodeToJson()
+  JsonDecodeIntoStruct()
+  MapEncodeToJson()
+  JsonDecodeIntoMap()
   fmt.Println("demo finish")
 }
+
+//struct的标签可以定义json解析时的一些操作
+//ArrNum []int //没有标签，表示被解析成的json键名直接就是ArrNum
+//ArrNum []int `json:"arr_num"` //这样表示，这个字段解析成的json键名是arr_num
+//ArrNum []int `json:"arr_num,omitempty"` //这样表示，如果struct中这个字段是空的就不解析
+//ArrNum []int `json:"-"` //这样表示，struct中这个字段不解析
 
 type ObjStruct struct {
   Num int    `json:"num"`
@@ -32,7 +39,8 @@ type TestStruct struct {
   Map    MapStruct   `json:"map"`
 }
 
-func jsonEncode() {
+//stract to json
+func StractEncodeToJson() {
   data := TestStruct{
     Num:    1,
     Str:    "str",
@@ -42,19 +50,38 @@ func jsonEncode() {
     Map:    MapStruct{NumKey: 1, StrKey: "str"},
   }
   jsonStr, _ := json.Marshal(data)
+  fmt.Println("stract to json")
   fmt.Println(string(jsonStr))
 }
 
-func jsonDecodeIntoMap() {
-  var data map[string]interface{}
-  jsonStr := "{\"num\":1,\"str\":\"str\",\"arr_num\":[1,2,3],\"arr_str\":[\"str1\",\"str2\",\"str3\"],\"arr_obj\":[{\"num\":1,\"str\":\"str1\"},{\"num\":2,\"str\":\"str2\"}],\"map\":{\"num_key\":1,\"str_key\":\"str\"}}"
-  json.Unmarshal([]byte(jsonStr), &data)
-  fmt.Println(data)
-}
-
-func jsonDecodeIntoStruct() {
+//json to stract
+func JsonDecodeIntoStruct() {
   var data TestStruct
   jsonStr := "{\"num\":1,\"str\":\"str\",\"arr_num\":[1,2,3],\"arr_str\":[\"str1\",\"str2\",\"str3\"],\"arr_obj\":[{\"num\":1,\"str\":\"str1\"},{\"num\":2,\"str\":\"str2\"}],\"map\":{\"num_key\":1,\"str_key\":\"str\"}}"
   json.Unmarshal([]byte(jsonStr), &data)
-  fmt.Println(data)
+  fmt.Println("json to stract")
+  fmt.Printf("%+v\r\n", data)
+}
+
+//map to json
+func MapEncodeToJson() {
+  data := make(map[string]interface{})
+  data["num"] = 1
+  data["str"] = "str"
+  data["arr_num"] = []int{1, 2, 3}
+  data["arr_str"] = []string{"str1", "str2", "str3"}
+  data["arr_obj"] = []ObjStruct{{Num: 1, Str: "str1"}, {Num: 2, Str: "str2"}}
+  data["map"] = MapStruct{NumKey: 1, StrKey: "str"}
+  jsonStr, _ := json.Marshal(data)
+  fmt.Println("map to json")
+  fmt.Println(string(jsonStr))
+}
+
+//json to map
+func JsonDecodeIntoMap() {
+  var data map[string]interface{}
+  jsonStr := "{\"num\":1,\"str\":\"str\",\"arr_num\":[1,2,3],\"arr_str\":[\"str1\",\"str2\",\"str3\"],\"arr_obj\":[{\"num\":1,\"str\":\"str1\"},{\"num\":2,\"str\":\"str2\"}],\"map\":{\"num_key\":1,\"str_key\":\"str\"}}"
+  json.Unmarshal([]byte(jsonStr), &data)
+  fmt.Println("json to map")
+  fmt.Printf("%+v\r\n", data)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module go0yang4li4
 
 go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.5.0
+)
