54cbd070e6ef275b52ee5a6a4c5bcfd471733905
KelipuTe <xhy_1365@sina.com>
2021-11-02 14:28:05 +0800

    demo

---
+++ demo/mongodb/mongodb.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "context"
+  "fmt"
+  "log"
+
+  "go.mongodb.org/mongo-driver/mongo"
+  "go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func main() {
+  clientOptions := options.Client().ApplyURI("mongodb://127.0.0.1:27017")
+  // Connect to MongoDB
+  client, err := mongo.Connect(context.TODO(), clientOptions)
+  if err != nil {
+    log.Fatal(err)
+  }
+  // Check the connection
+  err = client.Ping(context.TODO(), nil)
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println("Connected to MongoDB!")
+  err = client.Disconnect(context.TODO())
+  if err != nil {
+    log.Fatal(err)
+  }
+  fmt.Println("Connection to MongoDB closed.")
+}
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.15
 require (
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.5.0
+	go.mongodb.org/mongo-driver v1.7.3 // indirect
 )
